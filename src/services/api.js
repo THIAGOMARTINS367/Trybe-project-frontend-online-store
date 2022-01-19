80a7dd760bfa92518f3c599e61df521362862922
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-01-19 16:13:16 -0300

    Melhorias feitas para corrigir erros do lint - Requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,16 @@
 export async function getCategories() {
   const fetchCategories = await fetch('https://api.mercadolibre.com/sites/MLB/categories')
-  .then((element) => element.json())
-  .then((dados) => dados)
-  .catch((erro) => erro);
+    .then((element) => element.json())
+    .then((dados) => dados)
+    .catch((erro) => erro);
   return fetchCategories;
-};
+}
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
   const fetchResult = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
-  .then((element) => element.json())
-  .then((dados) => dados)
-  .catch((erro) => erro);
+    .then((element) => element.json())
+    .then((dados) => dados)
+    .catch((erro) => erro);
   return fetchResult;
 }
