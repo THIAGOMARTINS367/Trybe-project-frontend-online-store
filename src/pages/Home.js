617c9a0124cc2d9f2d4909764746f933f2947298
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-01-19 21:27:31 -0300

    Requisito 4 feito.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import AsideCategoriesMenu from '../components/AsideCategoriesMenu';
 import LabelAndInput from '../components/LabelAndInput';
 import iconShoppingCart from '../icons/carrinho-de-compras.png';
 
@@ -7,22 +8,25 @@ class Home extends Component {
   render() {
     return (
       <main>
-        <LabelAndInput
-          labelContent="Procurar"
-          inputID="input-search-product"
-          inputType="text"
-        />
-        <Link to="/shoppingcart" data-testid="shopping-cart-button">
-          <img
-            src={ iconShoppingCart }
-            alt="Icone Carrinho de Compras"
-            className="icon-shopping-cart"
+        <section className="section1">
+          <LabelAndInput
+            labelContent="Procurar"
+            inputID="input-search-product"
+            inputType="text"
           />
-        </Link>
-        <br />
-        <span data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </span>
+          <Link to="/shoppingcart" data-testid="shopping-cart-button">
+            <img
+              src={ iconShoppingCart }
+              alt="Icone Carrinho de Compras"
+              className="icon-shopping-cart"
+            />
+          </Link>
+          <br />
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+        </section>
+        <AsideCategoriesMenu />
       </main>
     );
   }
