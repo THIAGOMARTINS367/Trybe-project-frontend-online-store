afc86467f0337ad612aee47e6c0be85603a3be6f
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-01-20 18:26:27 -0300

    Requisito 6 feito.

--- src/components/AsideCategoriesMenu.js
+++ src/components/AsideCategoriesMenu.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { getCategories } from '../services/api';
 
 class AsideCategoriesMenu extends Component {
@@ -16,17 +17,38 @@ class AsideCategoriesMenu extends Component {
 
   render() {
     const { dataCategories } = this.state;
+    const { onChangeEvent } = this.props;
     return (
       <aside>
-        {dataCategories.map((element) => (
-          <label key={ element.id } htmlFor={ element.id } data-testid="category">
-            <input type="radio" id={ element.id } />
-            { element.name }
-          </label>
-        ))}
+        {
+          dataCategories.map((element) => (
+            <label
+              key={ element.id }
+              htmlFor={ element.id }
+              name="radioButton-for-category"
+              data-testid="category"
+            >
+              <input
+                type="radio"
+                id={ element.id }
+                name="radioButton-for-category"
+                onChange={ onChangeEvent }
+              />
+              { element.name }
+            </label>
+          ))
+        }
       </aside>
     );
   }
 }
+
+AsideCategoriesMenu.propTypes = {
+  onChangeEvent: PropTypes.func,
+};
+
+AsideCategoriesMenu.defaultProps = {
+  onChangeEvent: () => '',
+};
 
 export default AsideCategoriesMenu;
