3ac22c5d387199765ded43d735ac42a3de2b8931
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-02-28 17:35:44 -0300

    Inclusão do arquivo de build.

---
+++ build/precache-manifest.227596a63a9055f6a6f2d1d489c533fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db2311d3d96f689d8347ecc6a1d4f2f",
+    "url": "/Trybe-project-frontend-online-store/index.html"
+  },
+  {
+    "revision": "4638f5793b6ed5673b54",
+    "url": "/Trybe-project-frontend-online-store/static/css/main.8769838b.chunk.css"
+  },
+  {
+    "revision": "3b8a1d67150065ed3639",
+    "url": "/Trybe-project-frontend-online-store/static/js/2.03be4285.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Trybe-project-frontend-online-store/static/js/2.03be4285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4638f5793b6ed5673b54",
+    "url": "/Trybe-project-frontend-online-store/static/js/main.4b6f2112.chunk.js"
+  },
+  {
+    "revision": "a1bcf18393843fbbb169",
+    "url": "/Trybe-project-frontend-online-store/static/js/runtime-main.ee68748e.js"
+  }
+]);
